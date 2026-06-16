91a9293671c3775019c7c28b5acb7a445e9d965a
kendikadimas <dimas.fazrulfalah@mhs.unsoed.ac.id>
2026-06-16 18:50:22 +0700

    fix CORS: add pusatvillaid.com origins

--- pusatvillaid/config/cors.php
+++ pusatvillaid/config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
+        'https://pusatvillaid.com',
+        'https://www.pusatvillaid.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
