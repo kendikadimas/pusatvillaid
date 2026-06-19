2694e537bcb6e5d2f6b651b391eaae3423bb7819
kendikadimas <dimas.fazrulfalah@mhs.unsoed.ac.id>
2026-06-20 03:25:43 +0700

    revert: remove QRIS auto-create, add manually like bank methods

--- pusatvillaid/app/Http/Controllers/PaymentMethodController.php
+++ pusatvillaid/app/Http/Controllers/PaymentMethodController.php
@@ -12,18 +12,6 @@ class PaymentMethodController extends Controller
      */
     public function indexPublic(): JsonResponse
     {
-        // Auto-create QRIS if not exists
-        $qris = PaymentMethod::where('code', 'qris')->first();
-        if (! $qris) {
-            PaymentMethod::create([
-                'name' => 'QRIS',
-                'code' => 'qris',
-                'account_number' => '',
-                'account_name' => 'PusatVilla Indonesia',
-                'is_active' => true,
-            ]);
-        }
-
         $methods = PaymentMethod::where('is_active', true)->get();
 
         return response()->json($methods);
